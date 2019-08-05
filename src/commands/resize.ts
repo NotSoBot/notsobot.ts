a7a9fa2170028b460cca6db64a77395d0769ce8f
Dan <dansoul.cake@gmail.com>
2019-08-05 01:15:36 -0400

    Create resize.ts

---
+++ src/commands/resize.ts
@@ -0,0 +1,51 @@
+import { Command } from 'detritus-client';
+
+import { NotSoClient } from '../client';
+import { Parameters } from '../utils';
+
+
+export default (<Command.CommandOptions> {
+  name: 'resize',
+  args: [
+    {default: 2, name: 'size', type: 'float'},
+  ],
+  label: 'url',
+  type: Parameters.getImageUrl,
+  onBefore: (context) => {
+    const channel = context.channel;
+    return (channel) ? channel.canAttachFiles : false;
+  },
+  onCancel: (context) => context.reply('⚠ Unable to send files in this channel.'),
+  onBeforeRun: (context, args) => !!args.url,
+  onCancelRun: (context, args) => {
+    if (args.url === null) {
+      return context.reply('⚠ Unable to find that user or it was an invalid url.');
+    } else {
+      return context.reply('⚠ An attachment, mention, name, or url is required.');
+    }
+  },
+  run: async (context, args) => {
+    const notsoclient = <NotSoClient> context.commandClient;
+    try {
+      const resize = await notsoclient.api.imageResize({
+        size: args.size || 2,
+        url: args.url,
+        userId: context.user.id,
+      });
+
+      return context.reply({
+        content: `height: ${resize.headers['x-dimensions-height']}, width: ${resize.headers['x-dimensions-width']}`,
+        file: {
+          contentType: resize.headers['content-type'],
+          filename: `resized.${resize.headers['x-extension']}`,
+          data: resize.data,
+          name: 'file',
+        },
+      });
+    } catch(error) {
+      return context.reply(error.message || error.stack);
+    }
+  },
+  onError: (context, args, error) => console.error(error),
+  onTypeError: (context, error) => context.reply(error.stack),
+});
