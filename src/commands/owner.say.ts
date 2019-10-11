ba774089fdb40e762073397431b70a209c3f84b3
Christopher F <foxbot@protonmail.com>
2019-10-11 02:26:24 -0400

    add say command (#5)
    * cmd: add say command
    
    very important
    
    * lint: remove unused import

---
+++ src/commands/owner.say.ts
@@ -0,0 +1,38 @@
+import { Command } from 'detritus-client';
+
+import { CommandTypes } from '../constants';
+import { onRunError } from '../utils';
+
+export interface CommandArgs {
+  text: string,
+}
+
+export default (<Command.CommandOptions> {
+  name: 'say',
+  metadata: {
+    description: 'Have the bot say something (owner only bc exploits)',
+    examples: [
+      'say :spider:',
+    ],
+    type: CommandTypes.OWNER,
+    usage: 'say <text>'
+  },
+  ratelimits: [
+    {
+      duration: 5000,
+      limit: 5,
+      type: 'guild',
+    },
+    {
+      duration: 1000,
+      limit: 1,
+      type: 'channel',
+    },
+  ],
+  type: (value) => { return value; },
+  onBefore: (context) => context.user.isClientOwner,
+  run: async (context, args: CommandArgs) => {
+    return context.reply(args.text);
+  },
+  onRunError,
+});
