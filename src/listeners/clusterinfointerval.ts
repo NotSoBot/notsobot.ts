cd5d3f6c6095ef8f736584e3e18ed7f2c93f473e
Dan <dansoul.cake@gmail.com>
2022-05-02 01:08:25 -0600

    Update clusterinfointerval.ts

--- src/listeners/clusterinfointerval.ts
+++ src/listeners/clusterinfointerval.ts
@@ -1,4 +1,5 @@
 import { ClusterClient, ShardClient } from 'detritus-client';
+import { ClientEvents } from 'detritus-client/lib/constants';
 import { EventSubscription, Timers } from 'detritus-utils';
 
 import { Listener } from './listener';
@@ -9,108 +10,87 @@ import { RedisSpewer } from '../redis';
 import { RedisPayloads } from '../types';
 
 
+const MAX_TIME_BETWEEN = 1500;
+
 class ClusterInfoInterval extends Listener {
-  interval = new Timers.Interval();
-  intervalTime = 15000;
+  lastUpload = 0;
+
+  async maybeUploadInformation(cluster: ClusterClient, force: boolean = false): Promise<void> {
+    const lastUpload = this.lastUpload;
+    if (!force && Date.now() - lastUpload <= MAX_TIME_BETWEEN) {
+      return;
+    }
+
+    const shard = cluster.shards.first();
+    if (shard) {
+      try {
+        const usage = process.memoryUsage();
+
+        this.lastUpload = Date.now();
+        await putInfoDiscord({client: shard}, {
+          clusterId: cluster.clusterId,
+          ramUsage: usage.heapUsed + usage.external + Math.max(0, usage.rss - usage.heapTotal),
+          shardCount: cluster.shardCount,
+          shardsPerCluster: (cluster.manager) ? cluster.manager.clusterShardsPer : 1,
+          shards: cluster.shards.map((shard) => {
+            return {
+              shardId: shard.shardId,
+              status: shard.gateway.state,
+
+              applications: shard.applications.length,
+              channels: shard.channels.length,
+              channelThreads: shard.channels.filter((channel) => channel.isGuildThread).length,
+              emojis: shard.emojis.length,
+              events: shard.gateway.sequence,
+              guilds: shard.guilds.length,
+              members: shard.members.length,
+              memberCount: shard.guilds.reduce((x, guild) => x + guild.memberCount, 0),
+              messages: shard.messages.length,
+              permissionOverwrites: shard.channels.reduce((x, channel) => x + channel.permissionOverwrites.length, 0),
+              presences: shard.presences.length,
+              presenceActivities: shard.presences.reduce((x, presence) => x + presence.activities.length, 0),
+              roles: shard.roles.length,
+              stageInstances: shard.stageInstances.length,
+              typings: shard.typings.length,
+              users: shard.users.length,
+              voiceStates: shard.voiceStates.length,
+            };
+          }),
+        });
+      } catch(error) {
+        this.lastUpload = lastUpload;
+      }
+    }
+  }
 
   create(cluster: ClusterClient, redis: RedisSpewer) {
     const subscriptions: Array<EventSubscription> = [];
 
     {
       const subscription = redis.subscribe(RedisChannels.INFO_DISCORD_REQUEST, async (payload: RedisPayloads.InfoDiscordRequest) => {
-        const shard = cluster.shards.first();
-        if (shard) {
-          try {
-            const usage = process.memoryUsage();
-  
-            await putInfoDiscord({client: shard}, {
-              clusterId: cluster.clusterId,
-              ramUsage: usage.heapUsed + usage.external + Math.max(0, usage.rss - usage.heapTotal),
-              shardCount: cluster.shardCount,
-              shardsPerCluster: (cluster.manager) ? cluster.manager.clusterShardsPer : 1,
-              shards: cluster.shards.map((shard) => {
-                return {
-                  shardId: shard.shardId,
-                  status: shard.gateway.state,
-  
-                  applications: shard.applications.length,
-                  channels: shard.channels.length,
-                  channelThreads: shard.channels.filter((channel) => channel.isGuildThread).length,
-                  emojis: shard.emojis.length,
-                  events: shard.gateway.sequence,
-                  guilds: shard.guilds.length,
-                  members: shard.members.length,
-                  memberCount: shard.guilds.reduce((x, guild) => x + guild.memberCount, 0),
-                  messages: shard.messages.length,
-                  permissionOverwrites: shard.channels.reduce((x, channel) => x + channel.permissionOverwrites.length, 0),
-                  presences: shard.presences.length,
-                  presenceActivities: shard.presences.reduce((x, presence) => x + presence.activities.length, 0),
-                  roles: shard.roles.length,
-                  stageInstances: shard.stageInstances.length,
-                  typings: shard.typings.length,
-                  users: shard.users.length,
-                  voiceStates: shard.voiceStates.length,
-                };
-              }),
-            });
-          } catch(error) {
-  
-          }
-        }
+        this.maybeUploadInformation(cluster, true);
       });
       subscriptions.push(subscription);
     }
 
-    /*
-    this.interval.start(this.intervalTime, async () => {
-      const shard = cluster.shards.first();
-      if (shard) {
-        try {
-          const usage = process.memoryUsage();
-
-          await putInfoDiscord({client: shard}, {
-            clusterId: cluster.clusterId,
-            ramUsage: usage.heapUsed + usage.external + Math.max(0, usage.rss - usage.heapTotal),
-            shardCount: cluster.shardCount,
-            shardsPerCluster: 8,
-            shards: cluster.shards.map((shard) => {
-              return {
-                shardId: shard.shardId,
-                status: shard.gateway.state,
-
-                applications: shard.applications.length,
-                channels: shard.channels.length,
-                channelThreads: shard.channels.filter((channel) => channel.isGuildThread).length,
-                emojis: shard.emojis.length,
-                events: shard.gateway.sequence,
-                guilds: shard.guilds.length,
-                members: shard.members.length,
-                memberCount: shard.guilds.reduce((x, guild) => x + guild.memberCount, 0),
-                messages: shard.messages.length,
-                permissionOverwrites: shard.channels.reduce((x, channel) => x + channel.permissionOverwrites.length, 0),
-                presences: shard.presences.length,
-                presenceActivities: shard.presences.reduce((x, presence) => x + presence.activities.length, 0),
-                roles: shard.roles.length,
-                stageInstances: shard.stageInstances.length,
-                typings: shard.typings.length,
-                users: shard.users.length,
-                voiceStates: shard.voiceStates.length,
-              };
-            }),
-          });
-        } catch(error) {
-
-        }
+    if (cluster.shards.length) {
+      for (let [shardId, shard] of cluster.shards) {
+        const gatewaySubscription = shard.gateway.subscribe('state', ({state}) => {
+          this.maybeUploadInformation(cluster);
+        });
+        subscriptions.push(gatewaySubscription);
       }
-    });
-    */
+    } else {
+      const subscription = cluster.subscribe(ClientEvents.SHARD, ({shard}) => {
+        const gatewaySubscription = shard.gateway.subscribe('state', ({state}) => {
+          this.maybeUploadInformation(cluster);
+        });
+        subscriptions.push(gatewaySubscription);
+      });
+      subscriptions.push(subscription);
+    }
 
     return subscriptions;
-  }
-
-  stop(cluster: ClusterClient | ShardClient) {
-    super.stop(cluster);
-    this.interval.stop();
   }
 }
 
