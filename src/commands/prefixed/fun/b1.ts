b3d8c342d265c4c9fd8dbbfa389f7dec909a7ba4
Dan <31395415+cakedan@users.noreply.github.com>
2022-08-19 01:51:06 -0600

    Update b1.ts

--- src/commands/prefixed/fun/b1.ts
+++ src/commands/prefixed/fun/b1.ts
@@ -1,13 +1,7 @@
 import { Command, CommandClient } from 'detritus-client';
 
-<<<<<<< Updated upstream
-import { CDN } from '../../../api/endpoints';
-import { CommandCategories } from '../../../constants';
-import { editOrReply } from '../../../utils';
-=======
 import { CommandCategories } from '../../../constants';
 import { Formatter } from '../../../utils';
->>>>>>> Stashed changes
 
 import { BaseCommand } from '../basecommand';
 
@@ -15,29 +9,6 @@ import { BaseCommand } from '../basecommand';
 export const COMMAND_NAME = 'b1';
 
 export default class B1Command extends BaseCommand {
-<<<<<<< Updated upstream
-    constructor(client: CommandClient) {
-        super(client, {
-            name: COMMAND_NAME,
-
-            metadata: {
-                description: 'cool',
-                examples: [
-                    COMMAND_NAME,
-                ],
-                category: CommandCategories.FUN,
-                usage: '',
-            },
-        });
-    }
-
-    async run(context: Command.Context) {
-        return editOrReply(
-            context,
-            CDN.COMMAND_ASSETS_B1,
-        );
-    }
-=======
   constructor(client: CommandClient) {
     super(client, {
       name: COMMAND_NAME,
@@ -57,5 +28,4 @@ export default class B1Command extends BaseCommand {
   run(context: Command.Context, args: Formatter.Commands.FunB1.CommandArgs) {
     return Formatter.Commands.FunB1.createMessage(context, args);
   }
->>>>>>> Stashed changes
 }
